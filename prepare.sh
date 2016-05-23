d8a8027da843362c3f06298bdf4c722c3694c3a1
Simon Lelievre <simon@lelievre.me>
2016-05-23 11:34:57 +0200

    full install

--- prepare.sh
+++ prepare.sh
@@ -1,36 +1,11 @@
 [ -d openwrt ] || git clone --depth=1 --branch overthebox.cc https://github.com/simlelievre/overthebox-openwrt.git openwrt
 cd openwrt
+curl -s "$CONFIGURL" > '.config'
 
-cat > feeds.conf << EOF
-# empty
-EOF
-
-./scripts/feeds clean
 ./scripts/feeds update -a
-
-cat > .config << EOF
-CONFIG_TARGET_x86=y
-CONFIG_TARGET_x86_64=y
-CONFIG_TARGET_x86_64_Default=y
-CONFIG_DEVEL=y
-CONFIG_TOOLCHAINOPTS=y
-CONFIG_GLIBC_USE_VERSION_2_21=y
-CONFIG_GLIBC_VERSION="2.21"
-CONFIG_GLIBC_VERSION_2_21=y
-CONFIG_LIBC="glibc"
-CONFIG_LIBC_USE_GLIBC=y
-CONFIG_LIBC_VERSION="2.21"
-CONFIG_PACKAGE_libpthread=y
-CONFIG_PACKAGE_librt=y
-CONFIG_IB=y
-CONFIG_SDK=y
-CONFIG_STRIP_ARGS="--strip-all"
-CONFIG_TARGET_SUFFIX="gnu"
-CONFIG_USE_GLIBC=y
-CONFIG_USE_LIBSTDCXX=y
-CONFIG_USE_STRIP=y
-CONFIG_CCACHE=y
-EOF
+./scripts/feeds install -a -p overthebox
+./scripts/feeds install -p overthebox -f netifd
+./scripts/feeds install -p overthebox -f dnsmasq
+./scripts/feeds install -a
 
 make defconfig
-
