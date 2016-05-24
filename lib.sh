af858660cf52d6baa532be075c2ec4d82b1cb083
Simon Lelievre <simon.lelievre@corp.ovh.com>
2016-05-24 12:11:39 +0200

    rewrite lib

--- lib.sh
+++ lib.sh
@@ -1,20 +1,39 @@
-[ -d openwrt ] || git clone --depth=1 --branch overthebox.cc https://github.com/simlelievre/overthebox-openwrt.git openwrt
 
-mkdir openwrt/{build_dir,staging_dir,package}
-mv build_dir/host openwrt/
-mv staging_dir/host openwrt/
-mv tools openwrt/
-mv dl openwrt/
-mv package/feeds openwrt/package
+DIR="openwrt"
 
+function clone(){
+    [ -d $DIR ] || git clone --depth=1 --branch overthebox.cc https://github.com/simlelievre/overthebox-openwrt.git $DIR
+}
 
-cd openwrt
-curl -s "$CONFIGURL" > '.config'
+function restore_cache(){
+    mkdir $DIR/{build_dir,staging_dir,package}
+    mv build_dir/host $DIR/
+    mv staging_dir/host $DIR/
+    mv tools/* $DIR/
+    rm -fr tools
+    mv dl $DIR/
+    mv package/feeds $DIR/package
+}
 
-./scripts/feeds update -a
-./scripts/feeds install -a -p overthebox
-./scripts/feeds install -p overthebox -f netifd
-./scripts/feeds install -p overthebox -f dnsmasq
-./scripts/feeds install -a
+function backup_cache(){
+    cd
+    mv $DIR/build_dir/host build_dir/
+    mv $DIR/staging_dir/host staging_dir/
+    mv $DIR/tools .
+    mv $DIR/dl .
+    mv $DIR/package/feeds package/
+}
 
-make defconfig
+function load_config(){
+    curl -s "$CONFIGURL" > $DIR/.config
+}
+
+function set_feeds(){
+    cd $DIR
+    ./scripts/feeds update -a
+    ./scripts/feeds install -a -p overthebox
+    ./scripts/feeds install -p overthebox -f netifd
+    ./scripts/feeds install -p overthebox -f dnsmasq
+    ./scripts/feeds install -a
+}
+
